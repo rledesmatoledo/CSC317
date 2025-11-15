343188f01994cc04f1ddf55b931c921347699253
Roberto <rledesmatoledo@sfsu.edu>
2025-11-14 21:38:14 -0800

    Organize Assignment 4 into assignments folder

---
+++ assignments/assignment-4/hello.js
@@ -0,0 +1,10 @@
+const language = "JavaScript";
+
+const createMessage = (name = "Developer") => {
+    return `Hello, ${name}! Welcome to the exciting world of ${language}.`;
+};
+
+console.log(createMessage());
+
+
+
